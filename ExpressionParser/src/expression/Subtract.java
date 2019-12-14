479ed4c29a93c7d95b163e9b81dcd912681756b9
A-Dyu <58152674+A-Dyu@users.noreply.github.com>
2019-12-15 02:48:22 +0300

    ReUpload))

---
+++ ExpressionParser/src/expression/Subtract.java
@@ -0,0 +1,28 @@
+package expression;
+
+public class Subtract extends AbstractBinaryOperator {
+    public Subtract(CommonExpression a, CommonExpression b) {
+        super(a, b);
+    }
+
+    @Override
+    protected String getOperator() {
+        return " - ";
+    }
+
+    @Override
+    protected int operate(int a, int b) {
+        return a - b;
+    }
+
+    @Override
+    protected boolean isOrdered() {
+        return true;
+    }
+
+    @Override
+    public int getPriority() {
+        return 1;
+    }
+
+}
